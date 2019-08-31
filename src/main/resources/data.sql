3354ded3c0f000db9530a86493000c70b3921341
José Lucas <jose122black@gmail.com>
2019-08-31 18:57:14 -0300

    Learning to how use AuthenticationManagerBuilder, BCryptPasswordEncoder and UserDetailsService to authenticate with formLogin

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$Olrsy6.JXyyC6iY8.seC5.VMmVaEeVRg57TG8TEzTCtA61fd7HVKG');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
